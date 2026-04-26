ba3e92883ac2af869648565638d587d8c7c49102
kudima03 <kudima03@gmail.com>
2026-04-26 11:08:39 +0300

    Breaking changes satisfied

--- src/Pure.Chart.RelationalModel/AxisRelationalModel.cs
+++ src/Pure.Chart.RelationalModel/AxisRelationalModel.cs
@@ -6,16 +6,13 @@ namespace Pure.Chart.RelationalModel;
 
 public sealed record AxisRelationalModel : IAxisRelationalModel
 {
-    public AxisRelationalModel(IGuid id, IGuid chartId, IString legend)
+    public AxisRelationalModel(IGuid id, IString legend)
     {
         Id = id;
         Legend = legend;
-        ChartId = chartId;
     }
 
     public IGuid Id { get; }
-
-    public IGuid ChartId { get; }
 
     public IString Legend { get; }
 }
--- src/Tests/Pure.Chart.RelationalModel.Tests/AxisRelationalModelTests.cs
+++ src/Tests/Pure.Chart.RelationalModel.Tests/AxisRelationalModelTests.cs
@@ -12,37 +12,16 @@ public sealed record AxisRelationalModelTests
     public void InitializeGuidCorrectly()
     {
         IGuid guid = new Guid();
-        IAxisRelationalModel type = new AxisRelationalModel(
-            guid,
-            new Guid(),
-            new RandomString()
-        );
+        IAxisRelationalModel type = new AxisRelationalModel(guid, new RandomString());
 
         Assert.Equal(guid.GuidValue, type.Id.GuidValue);
-    }
-
-    [Fact]
-    public void InitializeChartIdCorrectly()
-    {
-        IGuid chartId = new Guid();
-        IAxisRelationalModel type = new AxisRelationalModel(
-            new Guid(),
-            chartId,
-            new RandomString()
-        );
-
-        Assert.Equal(chartId.GuidValue, type.ChartId.GuidValue);
     }
 
     [Fact]
     public void InitializeLegendCorrectly()
     {
         IString legend = new RandomString();
-        IAxisRelationalModel type = new AxisRelationalModel(
-            new Guid(),
-            new Guid(),
-            legend
-        );
+        IAxisRelationalModel type = new AxisRelationalModel(new Guid(), legend);
 
         Assert.Equal(legend.TextValue, type.Legend.TextValue);
     }
